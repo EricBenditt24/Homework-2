278070051aae4c3ad878cc54b06d3cecbe47014a
EricBenditt <erb5623@psu.edu>
2020-09-11 19:40:44 +0000

    Change the print sentence

--- main.py
+++ main.py
@@ -24,17 +24,17 @@ def run():
   lettergrade = input("Enter your course 1 letter grade: ")
   credit1 = int(input("Enter your course 1 credit: "))
   gradepoint1 = getGradePoint(lettergrade) 
-  print (f"The grade point for course 1 is: {gradepoint1}") 
+  print (f"Grade point for course 1 is: {gradepoint1}") 
 
   lettergrade = input("Enter your course 2 letter grade: ")
   credit2 = int(input("Enter your course 2 credit: "))
   gradepoint2 = getGradePoint(lettergrade) 
-  print (f"The grade point for course 2 is: {gradepoint2}")
+  print (f"Grade point for course 2 is: {gradepoint2}")
 
   lettergrade = input("Enter your course 3 letter grade: ")
   credit3 = int(input("Enter your course 3 credit: "))
   gradepoint3 = getGradePoint(lettergrade) 
-  print (f"The grade point for course 3 is: {gradepoint3}")
+  print (f"Grade point for course 3 is: {gradepoint3}")
 
   GPA = (gradepoint1 * credit1 + gradepoint2 * credit2 + gradepoint3 * credit3) / (credit1 + credit2 + credit3)
   print(f"Your GPA is: {GPA}")
